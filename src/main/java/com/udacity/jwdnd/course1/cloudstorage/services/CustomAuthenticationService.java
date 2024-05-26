f264099a28924c60f5332c2249d7634ec022afed
sngom1995 <ngomsamba75@gmail.com>
2024-05-26 00:20:06 +0000

    add auth service

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/CustomAuthenticationService.java
@@ -0,0 +1,42 @@
+package com.udacity.jwdnd.course1.cloudstorage.services;
+
+import com.udacity.jwdnd.course1.cloudstorage.domain.User;
+import com.udacity.jwdnd.course1.cloudstorage.mapper.UserMapper;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+
+import java.util.ArrayList;
+
+public class CustomAuthenticationService implements AuthenticationProvider {
+
+    private final UserMapper userMapper;
+    private final HashService hashService;
+
+    public CustomAuthenticationService(UserMapper userMapper, HashService hashService) {
+        this.userMapper = userMapper;
+        this.hashService = hashService;
+    }
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        User user = userMapper.findUserByUsername(username);
+        if (user != null) {
+            String encodedSalt = user.getSalt();
+            String hashedPassword = hashService.getHashedValue(password, encodedSalt);
+            if (user.getPassword().equals(hashedPassword)) {
+                return new UsernamePasswordAuthenticationToken(username, password, new ArrayList<>());
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public boolean supports(Class<?> auth) {
+        return auth.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
